4376ef7adcc215102b5ec46594a108891b1ae9b1
rodrvn <rodrigojvallejos@gmail.com>
2023-03-08 17:50:45 -0300

    database contected to flask

--- app.py
+++ app.py
@@ -1,35 +1,41 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from models import db, Usuario, Ingresos, Gastos, Ahorro
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# conecta la base de datos y conecta la app
+db.init_app(app)
+
 @app.route("/")
 def index():
+    return 'poner /id_usuario :)'
+
+@app.route("/<id_usuario>")
+def tracker(id_usuario):
+
+    # Trae los datos del usuario segun id
+    usuario = Usuario.query.get(id_usuario)
     
-    fullname = 'Pingunecia Del Rio'
+    #Trae los datos segun el id
+    ingresos = Ingresos.query.filter_by(id_usuario=id_usuario).all()
+    gastos = Gastos.query.filter_by(id_usuario=id_usuario).all()
+    ahorros = Ahorro.query.filter_by(id_usuario=id_usuario).all()
 
-    saldo_total = 50000
+    #Trae los datos de la database sumados y filtrados por la id
+  
+    ingresos_totales = db.session.query(db.func.sum(Ingresos.ingreso_reciente)).filter(Ingresos.id_usuario==id_usuario).scalar()
+    gastos_totales = db.session.query(db.func.sum(Gastos.gasto_reciente)).filter(Gastos.id_usuario==id_usuario).scalar()
+    ahorros = db.session.query(db.func.sum(Ahorro.monto_ahorro)).filter(Ahorro.id_usuario==id_usuario).scalar()
 
-    gastos_total = 20000
-
-    ahorro = 400500
-
-    razon_gasto = 'cena choripan'
-
-    gastos_recientes = 12000
-
-    ingresos_recientes = 60000
-
-    razon_ingreso = 'venta chat.gpt'
-
-    banco_efectivo= 'efectivo' #Variable para saber si el ingreso/egreso se hizo a traves de un banco o a traves de efectivo
-
-    fecha = '7/marzo/2023'
-
-
-    
+    #Hace el calculo del saldo total
+    saldo_total = (ingresos_totales - gastos_totales)
     
 
-    return render_template('base.html', fullname=fullname, saldo_total=saldo_total, gastos_total=gastos_total, ahorro=ahorro, gastos_recientes=gastos_recientes, ingresos_recientes=ingresos_recientes, fecha=fecha, razon_ingreso=razon_ingreso, razon_gasto=razon_gasto, banco_efectivo=banco_efectivo)
+    return render_template('base.html', usuario=usuario, ingresos=ingresos, gastos=gastos, ahorros=ahorros, saldo_total=saldo_total, gastos_totales=gastos_totales)
 
 
 
