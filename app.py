db76dcc925fdc2dfc54554e7601f48da561b9e0a
rodrvn <rodrigojvallejos@gmail.com>
2023-03-09 22:43:09 -0300

    agregar transaccion added

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from models import db, Usuario, Ingresos, Gastos, Ahorro
 
@@ -14,7 +14,7 @@ db.init_app(app)
 def index():
     return 'poner /id_usuario :)'
 
-@app.route("/<id_usuario>")
+@app.route("/tracker/<id_usuario>", methods=['GET'])
 def tracker(id_usuario):
 
     # Trae los datos del usuario segun id
@@ -32,11 +32,42 @@ def tracker(id_usuario):
     ahorros = db.session.query(db.func.sum(Ahorro.monto_ahorro)).filter(Ahorro.id_usuario==id_usuario).scalar()
 
     #Hace el calculo del saldo total
-    saldo_total = (ingresos_totales - gastos_totales)
+    
+    saldo_total = ((ingresos_totales) - (gastos_totales))
+    
+    print(saldo_total)
     
 
     return render_template('base.html', usuario=usuario, ingresos=ingresos, gastos=gastos, ahorros=ahorros, saldo_total=saldo_total, gastos_totales=gastos_totales)
 
+@app.route("/agregar_ingreso/<id_usuario>")
+def agregar_ingreso(id_usuario):
+
+    return 'enviado'
+
+@app.route("/agregar_gasto/<id_usuario>", methods=["POST", "GET"])
+def agregar_gastos(id_usuario):
+    gasto_reciente = request.form['gasto_reciente']
+    razon_gasto = request.form['razon_gasto']
+    # fecha_gasto = request.form['fecha_gasto']
+
+    gasto = Gastos(id_usuario=id_usuario, gasto_reciente=gasto_reciente, razon_gasto=razon_gasto)
+    db.session.add(gasto)
+    db.session.commit()
+    return redirect(url_for('tracker', id_usuario=id_usuario))
+
+@app.route("/editar/<id_usuario>")
+def editar(id_usuario):
+    return 'editar'
+
+@app.route("/eliminar/<id_usuario>")
+def borrar(id_usuario):
+    return 'borrar'
+    
+
+    
+
+    
 
 
 
